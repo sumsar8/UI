99104475b3343591dfcadae0937bddb59b78a6b1
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2021-02-23 11:16:52 +0100

    döden

--- src/Guiform.java
+++ src/Guiform.java
@@ -11,7 +11,7 @@ public class Guiform {
 
     private JPanel panel1;
     private JButton button1;
-    private JTextField textField1;
+    private JTextField textField2;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
